7af5f425dbbc8be8e750662769edb375c268e9f5
Ahmed Guedmiuoi <138891005+ahmed-guedmioui@users.noreply.github.com>
2024-09-11 10:55:34 +0100

    Add files via upload

---
+++ app/src/main/java/com/ag_apps/loginandregisteruitutorial/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.ag_apps.loginandregisteruitutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ag_apps.loginandregisteruitutorial.ui.theme.LoginAndRegisterUITutorialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LoginAndRegisterUITutorialTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    LoginScreen(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
